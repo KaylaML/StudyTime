c731de5300d3a66ad951c4237a53c23aefdcca0f
Kayla <118627199+KaylaML@users.noreply.github.com>
2023-06-05 08:28:20 +0100

    Update loop

--- StudyTime.py
+++ StudyTime.py
@@ -2,11 +2,14 @@ import time
 import random
 import winsound
 
-def timer():
+def set_timer():
     timer = 0
     interval = int(input("What intervals do you want to work at?\n")) * 60
     beep_time = random.randint(0, interval)
+    timer_loop(timer, interval, beep_time)
 
+
+def timer_loop(timer, interval, beep_time):
     while timer < interval:
         if beep_time == timer:
             winsound.Beep(1500, 500)
@@ -24,17 +27,18 @@ def timer():
         break_time(interval / 4)
 
 
-def reset_timer(timer):
+def break_time(break_timer):
     timer = 0
-
-def break_time(break_time):
-    timer = 0
-
-    while timer < break_time:
+    while timer < break_timer:
         time.sleep(1)
         timer += 1
         print(timer / 100)
     print("Time to work!")
-    
+    set_timer()
 
-timer()
+
+def reset_timer(timer):
+    timer = 0
+
+
+set_timer()
